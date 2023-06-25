7df1e37238834abb6a360fb5e05347e4bc3ed632
sorawitlertumpaisakulwong <sorawit@senestia.com>
2023-06-25 21:15:13 +0700

    refactor and add sqs

--- sync/s3_multipart_upload.py
+++ sync/s3_multipart_upload.py
@@ -18,7 +18,7 @@ def upload_progress_callback(bytes_transferred):
     print(f"Uploaded {progress_upload_mb} / {file_size_mb} mb")
 
 
-def upload_large_file_to_s3(bucket_name, file_path, key_name):
+def upload_large_file_to_s3(file_path, upload_key):
     global file_size_mb
 
     file_size = os.path.getsize(file_path)
@@ -34,25 +34,23 @@ def upload_large_file_to_s3(bucket_name, file_path, key_name):
     )
 
     config = TransferConfig(
-        max_concurrency=1,
+        max_concurrency=5,
         multipart_chunksize=int(env["PART_SIZE_IN_MB"]) * 1024 * 1024,
-        # io_chunksize=int(env["PART_SIZE_IN_MB"]) * 1024 * 1024,
-        use_threads=True
+        # use_threads=True
     )
 
     # Upload the file
     s3.upload_file(
         Filename=file_path,
-        Bucket=bucket_name,
-        Key=key_name,
+        Bucket=env["BUCKET_NAME"],
+        Key=upload_key,
         Config=config,
         Callback=upload_progress_callback
     )
 
 
 # Example usage
-bucket_name = env["BUCKET_NAME"]
 file_path = "/Users/sorawitlertumpaisakulwong/Desktop/poc-python-aws-localstack/Untitled.mov"
-key_name = env["UPLOAD_KEY"]
+upload_key = env["UPLOAD_KEY"]
 
-upload_large_file_to_s3(bucket_name, file_path, key_name)
+upload_large_file_to_s3(file_path, upload_key)
